6d2d9023166de57e96647771e0b7fbea82c8ae9a
Hritik Ghadage <ghadagehritik97@gmail.com>
2026-06-08 21:03:11 +0530

    reverse string problem

---
+++ src/mixedQuestions/ReverseString.java
@@ -0,0 +1,9 @@
+package mixedQuestions;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str=new String("Hello World");
+        StringBuilder br=new StringBuilder(str);
+        System.out.println(br.reverse());
+    }
+}
